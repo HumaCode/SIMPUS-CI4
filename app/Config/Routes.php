86d49da67762f0ffa428274dc30f71c252a0979b
humaidi <humaidizakaria1998@gmail.com>
2022-07-08 19:05:05 +0700

    Enabled auto routing

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -21,6 +21,7 @@ $routes->setDefaultController('Home');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
+$routes->setAutoRoute(true);
 // The Auto Routing (Legacy) is very dangerous. It is easy to create vulnerable apps
 // where controller filters or CSRF protection are bypassed.
 // If you don't want to define all routes, please use the Auto Routing (Improved).
@@ -37,10 +38,10 @@ $routes->set404Override();
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 
-$routes->get('admin', 'Admin::index');
+// $routes->get('admin', 'Admin::index');
 
 
-$routes->get('login', 'Login::index');
+// $routes->get('login', 'Login::index');
 
 /*
  * --------------------------------------------------------------------
